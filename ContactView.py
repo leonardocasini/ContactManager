cea75ba0f7731ab48adb9656670f21224b4c5c13
leonardocasini <leonardo.casini1@stud.unifi.it>
2018-06-13 16:28:16 +0200

    Add files via upload

---
+++ ContactView.py
@@ -0,0 +1,137 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtGui import QIcon, QPixmap
+
+class Ui_Form(object):
+    def setupUi(self, Form, name,surname,number,email,note):
+        Form.setObjectName("Contact View")
+        Form.resize(436, 333)
+        self.form  = Form
+
+        #Assign the values whith the contact values clicked
+        self.name = name
+        self.surname = surname
+        self.number = number
+        self.email = email
+        self.note=note
+        #A boolean attribute to make decision Save/Delete
+        self.deleteChoose  = False
+
+        #Label is filled with icon of a profile
+        self.iconLabel = QtWidgets.QLabel(Form)
+        pixmap = QPixmap('img/profile.png')
+        pixmap = pixmap.scaled(161, 161, transformMode=QtCore.Qt.SmoothTransformation)
+        self.iconLabel.setPixmap(pixmap)
+        self.iconLabel.setGeometry(QtCore.QRect(30, 10, 161, 161))
+        self.iconLabel.setObjectName("label")
+        
+        #QLabel show the field where make annotation
+        self.noteTextLabel = QtWidgets.QLabel(Form)
+        self.noteTextLabel.setGeometry(QtCore.QRect(20, 200, 71, 16))
+        self.noteTextLabel.setObjectName("noteTextLabel")
+        
+        #QTextEdit where insert some annotation about the contact
+        self.noteTextEdit = QtWidgets.QTextEdit(Form)
+        self.noteTextEdit.setGeometry(QtCore.QRect(103, 198, 311, 71))
+        self.noteTextEdit.setObjectName("textEdit")
+        #Initialize with the input value
+        self.noteTextEdit.append(self.note)
+        
+        self.widget = QtWidgets.QWidget(Form)
+        self.widget.setGeometry(QtCore.QRect(210, 20, 211, 151))
+        self.widget.setObjectName("widget")
+        self.formLayout = QtWidgets.QFormLayout(self.widget)
+        self.formLayout.setContentsMargins(0, 0, 0, 0)
+        self.formLayout.setObjectName("formLayout")
+        
+        #Label show where is nameLineEdit 
+        self.nameFieldLabel = QtWidgets.QLabel(self.widget)
+        self.nameFieldLabel.setObjectName("labelName")
+        self.formLayout.setWidget(0, QtWidgets.QFormLayout.LabelRole, self.nameFieldLabel)
+        
+        #LineEdit where user can modify name
+        self.nameLineEdit = QtWidgets.QLineEdit(self.widget)
+        self.nameLineEdit.setObjectName("nameLineEdit")
+        self.nameLineEdit.setText(self.name)
+        self.formLayout.setWidget(0, QtWidgets.QFormLayout.FieldRole, self.nameLineEdit)
+        
+        #Label show where is surnameLineEdit 
+        self.surnameFieldLabel = QtWidgets.QLabel(self.widget)
+        self.surnameFieldLabel.setObjectName("label_3")
+        self.formLayout.setWidget(1, QtWidgets.QFormLayout.LabelRole, self.surnameFieldLabel)
+        
+        #LineEdit where user can modify surname
+        self.surnameLineEdit = QtWidgets.QLineEdit(self.widget)
+        self.surnameLineEdit.setObjectName("lineEdit_2")
+        self.surnameLineEdit.setText(self.surname)
+        self.formLayout.setWidget(1, QtWidgets.QFormLayout.FieldRole, self.surnameLineEdit)
+        
+        #Label show where is numberLineEdit 
+        self.numberFieldLabel = QtWidgets.QLabel(self.widget)
+        self.numberFieldLabel.setObjectName("numberFieldLabel")
+        self.formLayout.setWidget(2, QtWidgets.QFormLayout.LabelRole, self.numberFieldLabel)
+        
+        #LineEdit where user can modify number
+        self.numberLineEdit = QtWidgets.QLineEdit(self.widget)
+        self.numberLineEdit.setObjectName("numberLineEdit")
+        self.numberLineEdit.setText(self.number)
+        self.formLayout.setWidget(2, QtWidgets.QFormLayout.FieldRole, self.numberLineEdit)
+        
+        #Label show where is emailLineEdit 
+        self.emailFieldLabel = QtWidgets.QLabel(self.widget)
+        self.emailFieldLabel.setObjectName("emailFieldLabel")
+        self.formLayout.setWidget(3, QtWidgets.QFormLayout.LabelRole, self.emailFieldLabel)
+        
+        #LineEdit where user can modify email
+        self.emailLineEdit = QtWidgets.QLineEdit(self.widget)
+        self.emailLineEdit.setObjectName("emailLineEdit")
+        self.emailLineEdit.setText(self.email)
+        self.formLayout.setWidget(3, QtWidgets.QFormLayout.FieldRole, self.emailLineEdit)
+
+        self.layoutWidget1 = QtWidgets.QWidget(Form)
+        self.layoutWidget1.setGeometry(QtCore.QRect(170, 290, 228, 32))
+        self.layoutWidget1.setObjectName("layoutWidget1")
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.layoutWidget1)
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        
+        #Button for the event of save changes
+        self.saveButton = QtWidgets.QPushButton(self.layoutWidget1)
+        self.saveButton.setObjectName("saveButton")
+        self.horizontalLayout.addWidget(self.saveButton)
+        
+        #Button for the event of delete Contact
+        self.deleteButton = QtWidgets.QPushButton(self.layoutWidget1)
+        self.deleteButton.setObjectName("pushButton_2")
+        self.horizontalLayout.addWidget(self.deleteButton)
+
+        self.retranslateUi(Form)
+        #The connection of method with the click events
+        self.saveButton.clicked.connect(self.saveClick)
+        self.deleteButton.clicked.connect(self.deleteClick)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+    
+    #This method sets True the deleteChoose attribute
+    def deleteClick(self):
+        self.deleteChoose  = True
+        self.form.accept()
+        
+    #Method that saves current value of the QLineEdits
+    def saveClick(self):
+        self.name = self.nameLineEdit.text()
+        self.surname = self.surnameLineEdit.text()
+        self.number = self.numberLineEdit.text()
+        self.email = self.emailLineEdit.text()
+        self.note = self.noteTextEdit.toPlainText()
+        self.form.accept()
+    
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.noteTextLabel.setText(_translate("Form", "Add Notes"))
+        self.nameFieldLabel.setText(_translate("Form", "Name"))
+        self.surnameFieldLabel.setText(_translate("Form", "Surname"))
+        self.numberFieldLabel.setText(_translate("Form", "Number"))
+        self.emailFieldLabel.setText(_translate("Form", "Mail"))
+        self.saveButton.setText(_translate("Form", "Save"))
+        self.deleteButton.setText(_translate("Form", "Delete"))
+
---
+++ main.py
@@ -0,0 +1,177 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.uic.properties import QtGui
+from NewContact import NewContForm
+from ContactView import Ui_Form
+
+import sys
+import qdarkstyle
+import pickle
+
+NAME, NUMBER, DATE, HIDDEN = range(4)
+
+#The Widget where add a new contact
+class NewContactForm(QtWidgets.QDialog, NewContForm):
+    def __init__(self,parent=None):
+        QtWidgets.QDialog.__init__(self,parent)
+        self.setupUi(self)
+
+#The Widget for the View of the contact
+class ContactViewForm(QtWidgets.QDialog, Ui_Form):
+    def __init__(self,parent=None, name=None,surname=None,number=None,email=None,note=None):
+        QtWidgets.QDialog.__init__(self,parent)
+        self.setupUi(self, name,surname,number,email,note)        
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("Contact Manager")
+        MainWindow.resize(369, 629)
+
+        #Database is implemented with the pickle module 
+        self.database = pickle.load(open( "save.p", "rb" ))
+        
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        
+        #Model with 4 columns (NAME+SURNAME,NUMBER,EMAIL,HIDDEN). The hidden column is used for the filter
+        self.model = QStandardItemModel(0, 4)
+        self.model.setHeaderData(NAME, Qt.Horizontal, "Name")
+        self.model.setHeaderData(NUMBER, Qt.Horizontal, "Number")
+        self.model.setHeaderData(DATE, Qt.Horizontal, "Email")
+
+        #The filter is created with the use of a QSortFilterProxyModel
+        self.mProxyModelMenu = QSortFilterProxyModel()
+        self.mProxyModelMenu.setDynamicSortFilter(True)
+        self.mProxyModelMenu.setSourceModel(self.model)
+        self.mProxyModelMenu.setFilterKeyColumn(3)
+
+        #Call the method that initialize the contact list (eventually loading an existent list)
+        self.generateList()
+
+        #QTreeView for the view of ContactList
+        self.tableWidget = QtWidgets.QTreeView(self.centralwidget)
+        self.tableWidget.setGeometry(QtCore.QRect(30, 50, 311, 491))
+        self.tableWidget.setObjectName("tableWidget")
+        self.tableWidget.setRootIsDecorated(False)
+        self.tableWidget.setAlternatingRowColors(True)
+        self.tableWidget.setModel(self.mProxyModelMenu)
+        self.tableWidget.setColumnHidden(3,True)
+        self.tableWidget.setSortingEnabled(True)
+        self.tableWidget.sortByColumn(0, QtCore.Qt.AscendingOrder)
+        self.tableWidget.resizeColumnToContents(0)
+        self.tableWidget.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        #The event of doubleclick on Row
+        self.tableWidget.doubleClicked.connect(self.editItem)
+
+        self.widget = QtWidgets.QWidget(self.centralwidget)
+        self.widget.setGeometry(QtCore.QRect(30, 10, 311, 33))
+        self.widget.setObjectName("widget")
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.widget)
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        
+        #Line where search contact
+        self.searchLineEdit = QtWidgets.QLineEdit(self.widget)
+        self.searchLineEdit.setObjectName("searchlineEdit")
+        #Connection of filtering model when press any key on searchLineEdit
+        self.searchLineEdit.textChanged.connect(self.filterRegExpChanged)
+        self.horizontalLayout.addWidget(self.searchLineEdit)
+
+        #The button for the Creation of a new Contact
+        self.newContactButton = QtWidgets.QPushButton(self.centralwidget)
+        self.newContactButton.setGeometry(QtCore.QRect(100, 560, 161, 51))
+        self.newContactButton.setObjectName("newContactButton")
+        self.newContactButton.clicked.connect(self.newContact)
+
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+    
+    def editItem(self):
+        currentRow = self.tableWidget.currentIndex().row()
+        text = self.tableWidget.selectedIndexes()[1].data(Qt.DisplayRole)
+        
+        #the order of contacts in the table is different in the database, so the programm search the contact
+        #with the same number of contact clicked 
+        for i in range(len(self.database)):
+            if text == self.database[i]['number']:
+                correctIndex = i
+        
+        # Once it found the correct index of contact it open the contactView        
+        cfv = ContactViewForm(None, self.database[correctIndex]['name'],self.database[correctIndex]['surname'],self.database[correctIndex]['number'],
+                                    self.database[correctIndex]['email'],self.database[correctIndex]['note'])
+        if cfv.exec_():
+            # if True the user clicks delete element button else he saves the modifies in the database
+            if cfv.deleteChoose: 
+                self.database.pop(correctIndex)
+            else:
+                self.database[correctIndex] = {'name': cfv.name,
+                                             'surname': cfv.surname,
+                                             'email': cfv.email,
+                                             'number': cfv.number,
+                                             'note':cfv.note}
+
+            #Load the changes of database in file.p
+            pickle.dump( self.database, open( "save.p", "wb" ) )
+            self.generateList()
+             
+    #this method set a filter for the SearchLineEdit
+    def filterRegExpChanged(self):
+        #The 0 value rappresents the choose of Regular Expression
+        syntax = QRegExp.PatternSyntax(0)
+        caseSensitivity = Qt.CaseSensitive
+        regExp = QRegExp(self.searchLineEdit.text(),
+                caseSensitivity, syntax)
+        self.mProxyModelMenu.setFilterRegExp(regExp)
+
+    #This method remove all rows and update with the element of database
+    def generateList(self):
+        self.model.removeRows( 0, self.model.rowCount() )
+        for i in range(len(self.database)):
+            self.model.insertRow(0)
+            self.model.setData(self.model.index(0, NAME), self.database[i]['name']+' '+self.database[i]['surname'])
+            self.model.setData(self.model.index(0, NUMBER), self.database[i]['number'])
+            self.model.setData(self.model.index(0, DATE), self.database[i]['email'])
+            self.model.setData(self.model.index(0, HIDDEN), self.database[i]['email']+self.database[i]['name']+
+                               self.database[i]['surname']+self.database[i]['number'])
+            
+    #this method open a Widget where insert the contact values 
+    def newContact(self):
+        ncf = NewContactForm(None)
+        if ncf.exec_():
+            self.model.insertRow(0)
+            self.model.setData(self.model.index(0, NAME), ncf.name +' '+ ncf.surname)
+            self.model.setData(self.model.index(0, NUMBER), ncf.number)
+            self.model.setData(self.model.index(0, DATE), ncf.email)
+            self.model.setData(self.model.index(0,HIDDEN), ncf.name +' '
+                               + ncf.surname+' '+ str(ncf.number)+' '+ ncf.email)
+            self.database = pickle.load( open( "save.p", "rb" ) )
+            self.database.append({'name': ncf.name, 
+                                  'surname': ncf.surname, 
+                                  'email': ncf.email,
+                                  'number': ncf.number,
+                                  'note':''})
+            pickle.dump( self.database, open( "save.p", "wb" ) )   
+            
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        __sortingEnabled = self.tableWidget.isSortingEnabled()
+        self.tableWidget.setSortingEnabled(False)
+        self.tableWidget.setSortingEnabled(__sortingEnabled)
+        self.newContactButton.setText(_translate("MainWindow", "New Contact"))
+
+
+if __name__ == '__main__':
+
+    app = QApplication(sys.argv)
+    window = QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(window)
+    app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
+    window.show()
+    sys.exit(app.exec_())        
